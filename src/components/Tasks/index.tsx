6617ea819a8bb5b3b567733fb0f9672733f0e3d5
Flávio <f.augustosdn.dev@gmail.com>
2023-10-17 23:12:09 -0300

    Implementado card de task

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,18 +1,22 @@
 import { View, Text } from "react-native"
 import { styles } from "./styles"
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { useState } from "react";
 
 export function Tasks() {
+  const [checkboxState, setCheckboxState] = useState(true)
+
   return (
     <View style={styles.container}>
       <BouncyCheckbox
         size={20}
         fillColor="#4EA8DE"
         unfillColor="transparent"
-        text="teste"
+        text="Integer urna interdum massa libero auctor neque turpis turpis semper."
         iconStyle={{ borderColor: "blue" }}
         innerIconStyle={{ borderWidth: 2 }}
-        onPress={(isChecked: boolean) => { }}
+        onPress={() => setCheckboxState(!checkboxState)}
+        textStyle={checkboxState ? styles.text : styles.textDisabled}
       />
     </View>
   )
--- src/components/Tasks/styles.ts
+++ src/components/Tasks/styles.ts
@@ -2,6 +2,16 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    padding: 12
+    padding: 12,
+    backgroundColor: '#262626',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#333333'
+  },
+  text: {
+    color: '#F2F2F2'
+  },
+  textDisabled: {
+    color: '#808080'
   }
 })
